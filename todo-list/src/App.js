56f20bee8827cd33214b18fde22286bf8d7a0964
offglitch <majidalturki@live.com>
2020-04-10 16:36:22 -0700

    layout

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import TodoItem from "./TodoItem"
+import todosData from "./todosData"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(id) {
+        // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
+        // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
+    }
+    
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+        
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )    
+    }
 }
 
-export default App;
+export default App
---
+++ todo-list/src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Feed the cats",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Catch up on The Office",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Complete To-Do List in React",
+        completed: false
+    }
+]
+
+export default todosData
